e565f9b008db5f926c4e11973196f6e8928d9dd4
tylerwp <tylerwp@gmail.com>
2016-08-26 09:59:16 -0400

    adding search

--- js/app.js
+++ js/app.js
@@ -2,6 +2,9 @@ var StudentsPerPage = 5;
 var PageCount = 0;
 var CurrentPage = 1;
 
+//temp 
+var studentSearchedList;
+
 var studentList = $('.student-list').children();
 studentList.hide();
 
@@ -20,9 +23,90 @@ var studentListChildrenFiltered = function(PerPage,CurPage){
     
     studentList.each(function(index,li){
         
+        //search functinailty here? if input has value?
+        
         var studentListItem = $(li);
         var pageIndexStart = PerPage*CurPage-PerPage;
         var pageIndexEnd = PerPage*CurPage;
+                
+        if(index >= pageIndexStart && index < pageIndexEnd){           
+            studentListItem.show();
+            console.log(index + ' ' + pageIndexStart + '-' + pageIndexEnd);        
+        }else{           
+            studentListItem.hide();       
+        }
+        
+    });    
+    paginationCreation();
+}
+
+//Create Pagination at bottom of student list
+var paginationCreation = function(){
+    
+    var page = $('.page');
+    var pagination = $("<div>").addClass('pagination');
+    var PagiUL = $("<ul>");  
+
+    for(i=0;i<studentList.length/StudentsPerPage;i++){
+        if(i == CurrentPage-1){
+            var PagiLI = $("<li>");
+            var PagiLink = $('<a href="#">');
+            PagiLink.text(i+1);
+            PagiUL.append(PagiLI.append(PagiLink.addClass("active")));
+            
+        }else{
+            var PagiLI = $("<li>");
+            var PagiLink = $('<a href="#">');
+            PagiLink.text(i+1);
+            //Add list items and click event to anchor tags
+            PagiUL.append(PagiLI.append(PagiLink.on("click",function(){
+                    //use clicked link's text as page value to update view
+                    var thePage = this;                             
+                    studentListChildrenFiltered(StudentsPerPage,thePage.innerText);
+               })));
+        }
+       
+    }
+    //remove pagination if exists
+    $(".pagination").remove();//find better way to update pagination?
+    pagination.append(PagiUL);    
+    page.append(pagination);
+}
+
+////////////////////////////////////////////////////////////////
+//Add search fields to element with class of page-header
+////////////////////////////////////////////////////////////////
+
+var studentSearch = $("<div>").addClass('student-search');
+studentSearch.append($('<input>').attr({placeholder:'Search for students...',id:'inputSearch'}));
+var searchButton = $('<button>').text('Search');
+studentSearch.append(searchButton.on('click',function(){
+    //Student Search
+    console.log($('#inputSearch').val());
+    
+    //returns h3 array?
+    //studentSearchedList = studentList.find("h3:contains(" + $('#inputSearch').val() + ")");
+    
+   // studentSearchedList = studentList.filter($('#inputSearch').val());
+    
+    //console.log($(studentSearchedList));
+    
+   
+    
+    
+    studentList.each(function(index,li){
+        var PerPage = 5;
+        var CurPage = 0;
+        var studentListItem = $(li);
+        var pageIndexStart = PerPage*CurPage-PerPage;
+        var pageIndexEnd = PerPage*CurPage;
+        
+       $(li).filter(function(){
+           
+         //  console.log($(this).filter($('#inputSearch').val()));
+           
+           
+       });
         
         if(index >= pageIndexStart && index < pageIndexEnd){
             studentListItem.show();
@@ -32,43 +116,17 @@ var studentListChildrenFiltered = function(PerPage,CurPage){
         }
         
     });    
-    paginationCreation();
-}
-
-var paginationCreation = function(){
-    var page = $('.page');
-    var pagination = $("<div>").addClass('pagination');
-    var PagiUL = $("<ul>");
     
-   // console.log(studentList.length/StudentsPerPage);
+    
+   
+    
+}))
+$('.page-header').append(studentSearch);
 
-    for(i=0;i<studentList.length/StudentsPerPage;i++){
-        if(i == CurrentPage-1){
-            var PagiLI = $("<li>");
-            var PagiLink = $("<a>");//need to add href="#"
-            PagiLink.text(i+1);
-            PagiUL.append(PagiLI.append(PagiLink.addClass("active")));
-            
-        }else{
-            var PagiLI = $("<li>");
-            var PagiLink = $("<a>");//need to add href="#"
-            PagiLink.text(i+1);
-           // PagiLink.on("click",clicktest());
-            PagiUL.append(PagiLI.append(PagiLink.on("click",function(){
-                var thePage = this; 
-                //console.log(thePage.innerText);                
-                studentListChildrenFiltered(StudentsPerPage,thePage.innerText);
-               })));
-        }
-       // console.log(i);
-    }
-    //remove pagination if exists
-    $(".pagination").remove();//find better way to update pagination?
-    pagination.append(PagiUL);
-    //page.replaceAll(".pagination");
-    page.append(pagination);
-}
+////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////
 
+//display students paginated list
 studentListChildrenFiltered(StudentsPerPage,1);
 
 
